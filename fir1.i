dd9d278de8c26c7bc2cd4c80f1ec6ec0d398d200
Bernd Porr <mail@berndporr.me.uk>
2018-11-02 10:29:22 +0000

    Added python wrapper (alpha)

---
+++ fir1.i
@@ -0,0 +1,17 @@
+%module fir1
+%{
+	#define SWIG_FILE_WITH_INIT
+	#include <stdio.h>
+	#include "Fir1.h"
+%}
+
+%include "numpy.i"
+
+%init %{
+    import_array();
+%}
+
+%apply (double* IN_ARRAY1, int DIM1) {(double *coefficients, unsigned number_of_taps)};
+
+
+%include "Fir1.h"
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+setup.py file for Fir1
+"""
+
+from setuptools import setup
+from setuptools import Extension
+import os
+from sys import platform
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if platform == "linux" or platform == "linux2":
+	fir1_module = Extension('_fir1',
+							sources=['fir1.i'],
+							extra_compile_args=['-std=c++11'],
+                                                        swig_opts=['-c++','-py3'],
+							)
+elif platform == "win32":
+	fir1_module = Extension('_fir1',
+							sources=['fir1.i'],
+							extra_compile_args=['/DWIN32_LEAN_AND_MEAN'],
+                                                        libraries=['ws2_32'],
+                                                        swig_opts=['-c++','-py3'],
+							)
+
+						   
+setup (name = 'fir1',
+       version = '1.3.0.0',
+       author      = "Bernd Porr",
+       author_email = "mail@berndporr.me.uk",
+       url = "https://github.com/berndporr/fir1",
+       description = 'Efficient FIR realtime filter',
+       long_description=read('README_py'),
+       ext_modules = [fir1_module],
+       py_modules = ["fir1"],
+       license='MIT',
+       classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: POSIX',
+	  'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python'
+          ]
+      )
